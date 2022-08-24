b72b49b870ed084f0e6839738e4a22abed0ad481
Palaash <goelpalaash@gmail.com>
2022-08-24 11:58:33 +0530

    Barlow SC font added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,10 @@ import styles from '../styles/Home.module.css'
 
 export default function Homepage() {
   return (
-  <h1 className ={`text-3xl font-bold underline ${styles.test}`}>HELLO WORLD!</h1>
+    <div className="h-screen bg-[#11161E] flex justify-center">
+      <h1 className = {`${styles.heading} text-7xl text-white font-medium mt-9`}>
+        Guess the country
+      </h1>
+    </div>
   )
 }
